c023abdd4a2bbd0e05c5dc537a53dd901dde27c4
Avy <avyu2006@outlook.com>
2024-04-25 16:06:31 -0400

    deleted random stuff

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,7 +43,4 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
     implementation("com.squareup.picasso:picasso:2.8")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
